c333e247e268d30b87d797e19ba9526d869f734c
Florian Lüdiger <florian.luediger@googlemail.com>
2022-02-18 20:44:16 +0100

    Fix a missing property in the SensorDataMapper

--- backend/src/main/kotlin/iot/poc/backend/influx/mapper/SensorDataMapper.kt
+++ backend/src/main/kotlin/iot/poc/backend/influx/mapper/SensorDataMapper.kt
@@ -1,5 +1,6 @@
 package iot.poc.backend.influx.mapper
 
+import com.influxdb.client.domain.WritePrecision
 import com.influxdb.client.write.Point
 import iot.poc.backend.dto.SensorData
 import org.springframework.stereotype.Component
@@ -7,6 +8,9 @@ import org.springframework.stereotype.Component
 @Component
 class SensorDataMapper {
     fun mapSensorDataToPoint(data: SensorData): Point {
-        return Point.measurement(data.sensorType).addTags(data.tags).addFields(data.values)
+        return Point.measurement(data.sensorType)
+            .time(data.timestamp.epochSecond, WritePrecision.S)
+            .addTags(data.tags)
+            .addFields(data.values)
     }
 }
